11a4009bd83bd7dbd84f77b042b9580df21cb542
Jye Robinson <jye@jrobi.co>
2020-07-26 14:24:42 +1000

    Fixed Conflict 2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log("Hello World!");
 
 function add(a, b) {
@@ -6,6 +5,4 @@ function add(a, b) {
 }
 
 console.log(add(5, 10));
-=======
 console.log("This is a merge conflict!");
->>>>>>> 4305615944a2fc56196fdea58507a689df148f91
